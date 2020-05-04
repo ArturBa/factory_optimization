59b5b3a447f749058252ebbd2be8272be7b7b397
wifilipiak <61492916+wifilipiak@users.noreply.github.com>
2020-05-04 20:58:22 +0200

    Reorder settings for better visibility

--- config.py
+++ config.py
@@ -1,14 +1,16 @@
-requirements = {'req_big': 20, 'req_small': 14, 'big_punish': 2, 'small_punish': 2}
-iterations = 100
-population_size = 10
-clone_rate = 0.01
+# Algorithm settings
+iterations = 200
+population_size = 200
+clone_rate = 0.5
 selection_rate = 0.2
+# Factory settings
 max_big_machines = 30
 max_small_machines = 30
 min_working_time = 1
 max_working_time = 16
 max_bonus = 0.5
 max_haste = 0.5
+requirements = {'req_big': 50, 'req_small': 40, 'big_punish': 40, 'small_punish': 35}
 
 # Watchdog for breaking learning process if no  progress
-watchdog = 9
+watchdog = 50
