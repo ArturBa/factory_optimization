e6e510bc58d8e3478f7cb6687367ca6f6509aa5c
Artur Bauer <bauerart1@gmail.com>
2020-05-06 11:28:34 +0200

    Update final raport and add use instruction

--- config.py
+++ config.py
@@ -1,8 +1,12 @@
 # Algorithm settings
 iterations = 200
-population_size = 200
+population_size = 100
 clone_rate = 0.5
-selection_rate = 0.2
+selection_rate = 0.5
+# Watchdog for breaking learning process if no  progress
+watchdog = 50
+
+
 # Factory settings
 max_big_machines = 30
 max_small_machines = 30
@@ -11,6 +15,3 @@ max_working_time = 16
 max_bonus = 0.5
 max_haste = 0.5
 requirements = {'req_big': 50, 'req_small': 40, 'big_punish': 40, 'small_punish': 35}
-
-# Watchdog for breaking learning process if no  progress
-watchdog = 50
